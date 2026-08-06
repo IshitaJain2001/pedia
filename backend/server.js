09f2abfaff7ae53a1d4b8ae71ff915816d6d11c3
ishita jain <ishitajain385@gmail.com>
2026-08-06 13:01:33 +0530

    --

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://pedia-9r1a.onrender.com', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   credentials: true
 }));
 app.use(express.json());
